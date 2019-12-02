51b90a6ea9c0982158a9ac6a21691ddd4a88cb2c
Nicolas Meinen <nicolas.meinen@consultant.volvo.com>
2019-12-02 17:15:56 +0100

    Resolve day2 - this one was really harder to do without mutation

--- two/intmachine.hs
+++ two/intmachine.hs
@@ -10,6 +10,8 @@ fromListRetrieve replacementList neededPosition = filter (isValidRepresentation 
 
 isValidRepresentation position testedValue = position == fstT testedValue
 
+transformToState values = zipWith (\x y -> (y, x, 0)) values [0..]
+
 -- make index for our list of triples checking whether 
 -- we should put the first one there or add a new one
 -- TODO make the next operation equal so we can chain them better. they are almost dasame goddamit
@@ -22,6 +24,9 @@ safeHead [] = Nothing
 safeHead (x:xs) = Just x
 
 -- usefull api (functions to call to represent state)
+
+-- argument invert cause I'm lazy
+fromListRetrieveLastInv neededPosition replacementList = fromListRetrieveLast replacementList neededPosition
 
 -- retrieves the current value or empty
 fromListRetrieveLast replacementList neededPosition = let filteredList = fromListRetrieve replacementList neededPosition in 
@@ -36,11 +41,33 @@ buildNewList currentList positionValue = let filteredList = fromListRetrieve cur
                                                let newTriple = [(fst positionValue, snd positionValue, nextIndex)] in
                                                  currentList ++ newTriple
 
+-- transform the opcode to the operation
+opcode (Just 1)  = Just (+)
+opcode (Just 2)  = Just (*)
+opcode (Just 99) = Nothing
+opcode (Just _) = Nothing
+opcode (Nothing) = Nothing
+
+buildNewListOnRec rewriteIndex currentList Nothing = currentList
+buildNewListOnRec rewriteIndex currentList (Just value) = buildNewList currentList (rewriteIndex, value)
+
+-- mutual rec function so we can actually end it
+operateRecurse Nothing _ _ list _ _ = list
+operateRecurse op left right list index (Just rewriteIndex) = let v = op <*> (fmap sndT left) <*> (fmap sndT right) in 
+                                                         findValueRec (index + 4) (buildNewListOnRec rewriteIndex list v)
 
 -- recurse on list untill we die
--- findValueRec [] state = state
--- findValueRec (x:xs) state = 
+findValueRec lookupIndex values = let retrievedValue = fromListRetrieveLast values lookupIndex in 
+                                    let selectedCode = opcode (fmap sndT retrievedValue) in
+                                      let leftIndex = fmap sndT (fromListRetrieveLast values (lookupIndex + 1)) in
+                                        let rightIndex = fmap sndT (fromListRetrieveLast values (lookupIndex + 2)) in
+                                          let newIndex = fmap sndT (fromListRetrieveLast values (lookupIndex + 3)) in
+                                            let (rightValue, leftValue) = (rightIndex >>= (fromListRetrieveLast values), leftIndex >>= (fromListRetrieveLast values)) in
+                                              operateRecurse selectedCode rightValue leftValue values lookupIndex newIndex
+
+findValueRecZero = findValueRec 0
 
 -- call rec starting with initial state and empty rewrite rules
-findValue initialState = findValueRec initialState []
+applyStateMachine = findValueRecZero . transformToState
 
+getLastNthArg arg = fmap sndT . fromListRetrieveLastInv arg . applyStateMachine 
